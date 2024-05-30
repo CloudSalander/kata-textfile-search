4e2aaa026714a3df2c39edded890853ae19aabfd
Cloud <ruben.as532@gmail.com>
2024-05-30 10:55:11 +0200

    Apparently clean text

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 define('FILE','loveIsAwful.txt');
+define('CHARS_TO_CLEAN',['.',',',';','!','?']);
+
+function cleanText(array $words): array {
+    
+    foreach($words as $key => $word) {
+        $words[$key] = str_replace(CHARS_TO_CLEAN,'',$word);
+    }
+    return $words;
+}
 
 $file = fopen(FILE,"r");
 $text = fread($file,filesize(FILE));
-var_dump(explode(" ",$text));
-
+$words = explode(" ",$text);
+$clean_words = cleanText($words);
+var_dump($clean_words);
 ?>
