d47c36f6827bdd01ed17d7cc9cf580b72eaafebb
Cloud <ruben.as532@gmail.com>
2024-05-30 11:07:10 +0200

    what I did in 30 minutes

--- index.php
+++ index.php
@@ -3,16 +3,24 @@ define('FILE','loveIsAwful.txt');
 define('CHARS_TO_CLEAN',['.',',',';','!','?']);
 
 function cleanText(array $words): array {
-    
     foreach($words as $key => $word) {
         $words[$key] = str_replace(CHARS_TO_CLEAN,'',$word);
     }
     return $words;
 }
 
+function printFirstLetterVowelWords(array $words): void {
+    $pattern = '/[aeiouAEIOU]/';
+    foreach($words as $word) {
+        if(preg_match($pattern, $word)) {
+            echo $word.PHP_EOL;
+        }
+    }
+}
+
 $file = fopen(FILE,"r");
 $text = fread($file,filesize(FILE));
 $words = explode(" ",$text);
 $clean_words = cleanText($words);
-var_dump($clean_words);
+printFirstLetterVowelWords($clean_words);
 ?>
